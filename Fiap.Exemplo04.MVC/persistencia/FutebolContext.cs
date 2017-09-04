5f2e4addbd17eaec0e6aff9f3eed39d294af18f0
logonrm <logonrm@fiap.com.br>
2017-09-04 20:51:58 -0300

    first commit

---
+++ Fiap.Exemplo04.MVC/persistencia/FutebolContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo04.MVC.persistencia
+{
+    public class FutebolContext : DbContext
+    {
+        public int TimeId { get; set; }
+        public string Nome{ get; set; }
+        public DateTime DataFundacao { get; set; }
+
+    }
+}
